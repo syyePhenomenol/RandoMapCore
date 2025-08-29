eeca4876827ea5db8b8d0082b54f78416d1a39ad
syyePhenomenol <yye3028@gmail.com>
2025-08-30 00:15:10 +0200

    Bump version

--- RandoMapCore/RandoMapCoreMod.cs
+++ RandoMapCore/RandoMapCoreMod.cs
@@ -49,7 +49,7 @@ public class RandoMapCoreMod : Mod, ILocalSettings<LocalSettings>, IGlobalSettin
 
     public override string GetVersion()
     {
-        return "1.0.15";
+        return "1.0.16";
     }
 
     public override int LoadPriority()
